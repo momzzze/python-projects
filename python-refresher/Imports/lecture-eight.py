62a3b26a1d79d4024c5584ca3af9aed44f8fad0f
nikola.ninov <momzzze@abv.bg>
2025-07-09 08:38:09 +0300

    Add grade_average_service.py and standardlib.py with functions for calculating homework grades and random selections

---
+++ python-refresher/Imports/lecture-eight.py
@@ -0,0 +1,10 @@
+import Imports.grade_average_service as grade_service
+
+homework_assignment_grades = {
+    "homework_1": 85,
+    "homework_2": 100,
+    "homework_3": 81,
+}
+
+# Imports.grade_average_service.calculate_homework(homework_assignment_grades)
+grade_service.calculate_homework(homework_assignment_grades)
---
+++ python-refresher/Imports/standartdib.py
@@ -0,0 +1,13 @@
+# Random
+import random
+
+types_of_drinks = ["water", "coffee", "tea", "juice", "soda"]
+
+print(random.choice(types_of_drinks))  # Randomly selects a drink from the list
+print(random.randint(1, 10))
+
+
+import math
+
+square_root = math.sqrt(64)
+print(square_root)
